c64b33534602b2d6c50af7a8c3d9d95ba29e9f0f
mahendra <153123561+mahendra8432@users.noreply.github.com>
2024-03-15 12:46:09 +0000

    Component Header Done

--- Contect/src/App.jsx
+++ Contect/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navigation from './Components/Navigation/Navigation'
+import ContactHeader from './Components/ContectHeader/ContectHeader'
 
 function App() {
 
   return (
     <div>
        <Navigation/>
+       <ContactHeader />
 
     </div>
   )
